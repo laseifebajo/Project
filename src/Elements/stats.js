8536c271a31ce1a65ff743896b636296c964edf0
laseifebajo <61692557+laseifebajo@users.noreply.github.com>
2022-01-03 15:36:38 +0000

    Update stats.js

--- src/Elements/stats.js
+++ src/Elements/stats.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Statsinfo } from './statsinfo';
 
 export class Stats extends React.Component{
-
+//the mapping here is  used to iterate over an array and calling function on every element of array.
     render(){
         return this.props.stats.map((stat) =>{
             return <Statsinfo stat={stat} ReloadData={this.props.ReloadData}></Statsinfo>
